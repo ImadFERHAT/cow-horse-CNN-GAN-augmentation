2c6e3682a5724b724f07543cd5aa8567db7d6bcf
ImadFERHAT <ferhatimadeddine@gmail.com>
2025-08-14 16:11:03 +0200

    Create generator.py

---
+++ cyclegan/generator.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+
+class ResidualBlock(nn.Module):
+    def __init__(self, features):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(features, features, kernel_size=3),
+            nn.InstanceNorm2d(features),
+            nn.ReLU(inplace=True),
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(features, features, kernel_size=3),
+            nn.InstanceNorm2d(features)
+        )
+
+    def forward(self, x):
+        return x + self.block(x)
+
+class Generator(nn.Module):
+    def __init__(self, in_channels=3, out_channels=3, n_residuals=6):
+        super().__init__()
+        model = [
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(in_channels, 64, kernel_size=7),
+            nn.InstanceNorm2d(64),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(64, 128, 3, stride=2, padding=1),
+            nn.InstanceNorm2d(128),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(128, 256, 3, stride=2, padding=1),
+            nn.InstanceNorm2d(256),
+            nn.ReLU(inplace=True),
+        ]
+
+        for _ in range(n_residuals):
+            model.append(ResidualBlock(256))
+
+        model += [
+            nn.ConvTranspose2d(256, 128, 3, stride=2, padding=1, output_padding=1),
+            nn.InstanceNorm2d(128),
+            nn.ReLU(inplace=True),
+
+            nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1, output_padding=1),
+            nn.InstanceNorm2d(64),
+            nn.ReLU(inplace=True),
+
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(64, out_channels, kernel_size=7),
+            nn.Tanh()
+        ]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        return self.model(x)
